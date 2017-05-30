f7514035c0b7fe2c510e4bb36fe8a45d3235b44f
chongtan111200 <chongtan111200@gmail.com>
2017-05-29 22:32:42 -0400

    for assignment 3 2

--- src/proxyPattern/ProxyPatternDemo.java
+++ src/proxyPattern/ProxyPatternDemo.java
@@ -2,7 +2,7 @@ package proxyPattern;
 
 public class ProxyPatternDemo {
 	public static void main(String[] args) {
-		ReadProxyDictionary.challenge("this");
-		ReadProxyDictionary.challenge("this");
+		ReadProxyDictionary.challenge("th");
+		ReadProxyDictionary.challenge("th");
 	}
 }
